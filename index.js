c7d898db5d71fd3dfe8fd680a724e366306584f9
André Baptista <aca.baptista@gmail.com>
2019-11-19 18:08:34 +0000

    ex2, 3 and tests

--- index.js
+++ index.js
@@ -1,8 +1,45 @@
 function ex1(){
-    let number = 50 + (25*2);
-    let result = number/100;
+	let number = 50 + (25*2);
+	let result = number/100;
 
-    return "This is exercise " + result + "!";
+	return "This is exercise " + result + "!";
 }
 
-console.log(ex1());
+function ex2() {
+	const x = 20;
+
+	if (x === 19);
+	{
+		return 'Incorrect';
+	}
+
+	return 'Correct';
+}
+
+function ex3() {
+	var x = 0;
+
+	if (x = 0) {
+		return 'Correct'
+	}
+	else {
+		return 'Incorrect'
+	}
+}
+
+function test1() {
+	return ex1() === 'This is exercise 1!';
+}
+
+function test2() {
+	return ex2() === 'Correct';
+}
+
+function test3() {
+	return ex3() === 'Correct';
+}
+
+/* eslint-disable no-console */
+console.log('Test 1 passed?: ', test1());
+console.log('Test 2 passed?: ', test2());
+console.log('Test 3 passed?: ', test3());
