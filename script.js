0685c465f0507054f7a95fd12978316f4ae904e2
André Baptista <aca.baptista@gmail.com>
2019-11-19 23:38:43 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 /* eslint-disable */
 
-function updateFirstName(firstname){
+function updateFirstName(firstname) {
     document.getElementById("firstNameInput").innerHTML = firstname;
 }
 
-function updateLastName(lastname){
+function updateLastName(lastname) {
     document.getElementById("lastNameInput").innerHTML = lastname;
 }
 
-document.getElementById('updateBtn').addEventListener("click", function(){
+document.getElementById('updateBtn').addEventListener("click", function() {
     eval('updateFirstName(" ' + unescape(document.getElementById("firstname").value) + ' ");')
     eval('updateLastName(" ' + unescape(document.getElementById("lastname").value) + ' ");')
 });
