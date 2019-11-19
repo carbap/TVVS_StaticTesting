bda60fc6c11010bbbf6ff88f48e6068bd0c4214b
francismaria <franciscomaria97@hotmail.com>
2019-11-19 18:51:07 +0000

    Add bug

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-function ex1(){
-    console.log("a");
+
+/* eslint-disable */
+
+function updateFirstName(firstname){
+    document.getElementById("firstNameInput").innerHTML = firstname;
 }
+
+function updateLastName(lastname){
+    document.getElementById("lastNameInput").innerHTML = lastname;
+}
+
+document.getElementById('updateBtn').addEventListener("click", function(){
+    eval('updateFirstName(" ' + unescape(document.getElementById("firstname").value) + ' ");')
+    eval('updateLastName(" ' + unescape(document.getElementById("lastname").value) + ' ");')
+});
