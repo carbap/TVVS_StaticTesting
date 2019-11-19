021849ae74e44e001fb3b9c1937519d4324db9e7
André Baptista <aca.baptista@gmail.com>
2019-11-19 22:51:14 +0000

    finish problems

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function ex1(){
+function ex1() {
 	let number = 50 + (25*2);
 	let result = number/100;
 
@@ -30,13 +30,26 @@ function ex3() {
 function ex4(n) {
 	const result = [0, 1];
 
-	for (var i = 2; i < n; i++) {
+	for (var i = 2; i<n; i++) {
 		result.push(result[i-2] + result[i-1]);
 	}
-	
+
 	let nth_number = result[n-1];
-	return result[n-1]
+	
+	return result[n-1];
 }
+
+/*
+function ex5() {
+	var j = 0;
+	var result = 0;
+
+	for(j; j < 10; j--)
+		result += j
+
+	return result
+}
+*/
 
 function test1() {
 	return ex1() === 'This is exercise 1!';
@@ -54,8 +67,13 @@ function test4() {
 	return ex4(9) === 21;
 }
 
+function test5() {
+	return ex5() === 45;
+}
+
 /* eslint-disable no-console */
 console.log('Test 1 passed?: ', test1());
 console.log('Test 2 passed?: ', test2());
 console.log('Test 3 passed?: ', test3());
 console.log('Test 4 passed?: ', test4());
+// console.log('Test 5 passed?: ', test5());
