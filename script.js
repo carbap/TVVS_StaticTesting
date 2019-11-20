603f2e63dc43c2e9e5099ad625948f40ef5c93b5
francismaria <franciscomaria97@hotmail.com>
2019-11-20 00:24:17 +0000

    Update script

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 /* eslint-disable */
-
 function updateFirstName(firstname){
     console.log(firstname + " firstname")
     document.getElementById("firstNameInput").innerHTML = firstname;
 }
 
-function updateLastName(lastname){
+function updateLastName(lastname) {
     document.getElementById("lastNameInput").innerHTML = lastname;
 }
 
@@ -13,5 +12,3 @@ document.getElementById('updateBtn').addEventListener("click", function(){
     eval('updateFirstName(" ' + document.getElementById("firstname").value + ' ");')
     eval('updateLastName(" ' + document.getElementById("lastname").value + ' ");')
 });
-
-// unescape(document.getElementById("firstname").value)
